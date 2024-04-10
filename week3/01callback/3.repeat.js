67203a68280557f50426870696d54c01c2172ca0
siwon <muncool39@gmail.com>
2024-04-11 05:18:11 +0900

    week3: practice callback repeat

--- week3/01callback/3.repeat.js
+++ week3/01callback/3.repeat.js
@@ -1,20 +1,11 @@
 // repeat 
 
+// 콜백함수 사용하지 않는 경우
 function repeat(count) {
     for(let idx=1;idx<=count;idx++) {
         console.log(idx);
     }
 }
-repeat(5);
-
-/*실행결과
-1
-2
-3
-4
-5
-*/
-
 function repeatDouble(count) {
     for(let idx=1;idx<=count;idx++){
         console.log(idx*2);
@@ -22,10 +13,28 @@ function repeatDouble(count) {
 }
 repeatDouble(5)
 
-/*실행결과
-2
-4
-6
-8
-10
-*/
+
+// 콜백함수 사용 (인자로 함수 받음)
+function repeat(count, callback) {
+	for (let idx = 1; idx <= count; idx++) {
+		callback(idx);
+	}
+}
+
+repeat(5, function (idx) {
+	console.log(idx);
+});
+repeat(5, ()=>console.log(idx));
+
+// Double
+repeat(5, function (idx) {
+	console.log(idx * 2);
+});
+repeat(5, ()=>console.log(idx*2));
+
+// Tripple
+repeat(5, function (idx) {
+	console.log(idx * 3);
+});
+repeat(5, ()=>console.log(idx*3));
+
