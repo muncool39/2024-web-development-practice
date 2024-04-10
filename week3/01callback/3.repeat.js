2c0980f29f9e39b64f1a394c9051311262d8cd59
siwon <muncool39@gmail.com>
2024-04-11 05:05:58 +0900

    week3: practice callback

---
+++ week3/01callback/3.repeat.js
@@ -0,0 +1,31 @@
+// repeat 
+
+function repeat(count) {
+    for(let idx=1;idx<=count;idx++) {
+        console.log(idx);
+    }
+}
+repeat(5);
+
+/*실행결과
+1
+2
+3
+4
+5
+*/
+
+function repeatDouble(count) {
+    for(let idx=1;idx<=count;idx++){
+        console.log(idx*2);
+    }
+}
+repeatDouble(5)
+
+/*실행결과
+2
+4
+6
+8
+10
+*/
